21ec56f88f7a220e3851f1fe11abeeafc373a30b
dbenitez <diegobm13650396@gmail.com>
2019-04-09 00:31:55 -0500

    first commit

---
+++ app/src/main/java/mx/com/ngv/ngdb/loginActivity/LoginActivity.java
@@ -0,0 +1,15 @@
+package mx.com.ngv.ngdb.loginActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mx.com.ngv.ngdb.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
